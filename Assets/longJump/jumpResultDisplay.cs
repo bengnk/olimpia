30e1822dc1730ad68fa5113b4ebefe1e3819011b
Jannic Föcker <jafoe2@gmail.com>
2025-03-06 11:13:27 +0100

    save Highscore

--- Assets/longJump/jumpResultDisplay.cs
+++ Assets/longJump/jumpResultDisplay.cs
@@ -10,14 +10,20 @@ public class JumpResultDisplay : MonoBehaviour
 
     // Array von 10 Texten für die Gegnerergebnisse
     public Text[] opponentResultTexts = new Text[5];  // Referenzen für die 10 Textfelder
-    public Text[] playerTexts = new  Text[5];
+    public Text[] playerTexts = new Text[5];
 
     // Buttons für Neustart und Hauptmenü
     public Button restartButton;  // Der Button für das Neustarten
     public Button mainMenuButton;  // Der Button für das Hauptmenü
 
+    // Highscore UI-Text
+    public Text highscoreText;  // Referenz für das Textfeld, das den Highscore anzeigt
+
     public AudioSource finishSound;
     private bool finishSoundPlayed = false;
+
+    // Schlüssel für den Highscore
+    private string highscoreKey = "Highscore";
 
     void Start()
     {
@@ -30,97 +36,130 @@ public class JumpResultDisplay : MonoBehaviour
         // Verknüpfe die Button-Events mit den Methoden
         restartButton.onClick.AddListener(RestartGame);
         mainMenuButton.onClick.AddListener(GoToMainMenu);
+
+        // Setze den Highscore zu Beginn
+        DisplayHighscore();
     }
 
     // Zeige die Sprungweite des Spielers und die Sprungweiten der Gegner an
     public void ShowJumpResults(string playerDistance, List<string> opponentResults)
-{
-    if (resultsCanvas != null)
     {
-        List<float> allResults = new List<float>();
-        List<string> foulResults = new List<string>(); // Speichert Fouls separat
-
-        // Spieler-Sprungweite zur Liste hinzufügen
-        if (playerDistance == "0,00" || playerDistance == "Foul") 
+        if (resultsCanvas != null)
         {
-            //playerJumpText.text = playerDistance;
-            foulResults.Add("Foul");
-        }
-        else if (float.TryParse(playerDistance, out float playerScore))
-        {
-            allResults.Add(playerScore);
-            //playerJumpText.text = playerDistance + " m";
-        }
+            List<float> allResults = new List<float>();
+            List<string> foulResults = new List<string>(); // Speichert Fouls separat
 
-        // Gegner-Sprungweiten verarbeiten
-        foreach (string opponent in opponentResults)
-        {
-
-            if (opponent == "Foul") 
+            // Spieler-Sprungweite zur Liste hinzufügen
+            if (playerDistance == "0,00" || playerDistance == "Foul")
             {
                 foulResults.Add("Foul");
             }
-            else if (float.TryParse(opponent, out float result))
+            else if (float.TryParse(playerDistance, out float playerScore))
             {
-                allResults.Add(result);
+                allResults.Add(playerScore);
+                playerJumpText.text = playerDistance + " m";  // Spieler-Sprungweite anzeigen
             }
-        }
 
-        // Ergebnisse nach Größe sortieren (höchster Wert zuerst)
-        allResults.Sort((a, b) => b.CompareTo(a));
-
-        // Ergebnisse in die UI eintragen
-        int index = 0;
-        int count = 0;
-        int count2 = 0;
-        int tmp = 0;
-        foreach (float result in allResults)
-        {
-            if (index < opponentResultTexts.Length)
+            // Gegner-Sprungweiten verarbeiten
+            foreach (string opponent in opponentResults)
             {
-                if (float.TryParse(playerDistance, out float playerDistanceFloat) && result == playerDistanceFloat && count == 0) {
-                    opponentResultTexts[index].text = result.ToString("F2") + " m";
-                    playerTexts[index].text = "Du";
-                    count++; // damit nicht mehrfach Du angezeigt werden kann
-                } else {
-                    tmp++;
-                    opponentResultTexts[index].text = result.ToString("F2") + " m";
-                    playerTexts[index].text = "Gegner " + tmp;
+                if (opponent == "Foul")
+                {
+                    foulResults.Add("Foul");
                 }
-                
-                index++;
-            }
-        }
-
-        // Foul-Ergebnisse hinten in die Liste eintragen
-        foreach (string foul in foulResults)
-        {
-            if (index < opponentResultTexts.Length)
-            {   
-                if ((playerDistance == "Foul" || playerDistance == "0,00") && count2 == 0) {
-                    playerTexts[index].text = "Du";
-                    opponentResultTexts[index].text = "Foul";
-                    count2++;
-                } else {
-                    playerTexts[index].text = "Gegner " + index;
-                    opponentResultTexts[index].text = "Foul";
+                else if (float.TryParse(opponent, out float result))
+                {
+                    allResults.Add(result);
                 }
-                
-                index++;
             }
-        }
 
-        // Nach 5 Sekunden das Canvas anzeigen
-        Invoke("DisplayCanvas", 5f);
+            // Ergebnisse nach Größe sortieren (höchster Wert zuerst)
+            allResults.Sort((a, b) => b.CompareTo(a));
+
+            // Ergebnisse in die UI eintragen
+            int index = 0;
+            int count = 0;
+            int count2 = 0;
+            int tmp = 0;
+            foreach (float result in allResults)
+            {
+                if (index < opponentResultTexts.Length)
+                {
+                    if (float.TryParse(playerDistance, out float playerDistanceFloat) && result == playerDistanceFloat && count == 0)
+                    {
+                        opponentResultTexts[index].text = result.ToString("F2") + " m";
+                        playerTexts[index].text = "Du";
+                        count++; // damit nicht mehrfach Du angezeigt werden kann
+                    }
+                    else
+                    {
+                        tmp++;
+                        opponentResultTexts[index].text = result.ToString("F2") + " m";
+                        playerTexts[index].text = "Gegner " + tmp;
+                    }
+
+                    index++;
+                }
+            }
+
+            // Foul-Ergebnisse hinten in die Liste eintragen
+            foreach (string foul in foulResults)
+            {
+                if (index < opponentResultTexts.Length)
+                {
+                    if ((playerDistance == "Foul" || playerDistance == "0,00") && count2 == 0)
+                    {
+                        playerTexts[index].text = "Du";
+                        opponentResultTexts[index].text = "Foul";
+                        count2++;
+                    }
+                    else
+                    {
+                        playerTexts[index].text = "Gegner " + index;
+                        opponentResultTexts[index].text = "Foul";
+                    }
+
+                    index++;
+                }
+            }
+
+            // Highscore prüfen und speichern
+            if (float.TryParse(playerDistance, out float playerScoreFinal))
+            {
+                CheckAndSaveHighscore(playerScoreFinal);  // Überprüfen und speichern, falls notwendig
+            }
+
+            // Nach 5 Sekunden das Canvas anzeigen
+            Invoke("DisplayCanvas", 5f);
+        }
     }
-}
 
+    // Methode, um den Highscore zu überprüfen und zu speichern
+    private void CheckAndSaveHighscore(float newScore)
+    {
+        float currentHighscore = PlayerPrefs.GetFloat(highscoreKey, 0f);  // Der aktuell gespeicherte Highscore
+
+        if (newScore > currentHighscore)
+        {
+            PlayerPrefs.SetFloat(highscoreKey, newScore);  // Neuer Highscore speichern
+            PlayerPrefs.Save();  // Speichern erzwingen
+            Debug.Log("Neuer Highscore: " + newScore);
+        }
+    }
+
+    // Methode, um den Highscore anzuzeigen
+    private void DisplayHighscore()
+    {
+        float highscore = PlayerPrefs.GetFloat(highscoreKey, 0f);  // Highscore laden
+        highscoreText.text = "Highscore: " + highscore.ToString("F2") + " m";  // Highscore im Textfeld anzeigen
+    }
 
     // Methode, um das Canvas anzuzeigen
     private void DisplayCanvas()
     {
         resultsCanvas.gameObject.SetActive(true);
-        if(!finishSoundPlayed) {
+        if (!finishSoundPlayed)
+        {
             finishSoundPlayed = true;
             finishSound.Play();
         }
